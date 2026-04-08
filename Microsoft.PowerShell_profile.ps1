4d0f27eec9bf4f6e92dd92bdba5176c0c3a99bfb
erik <eriksokolov@outlook.com>
2026-04-08 20:35:19 +0200

    04/08/2026 20:35:19

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,23 @@ function Upload-Git
 
 
 Set-PSReadLineOption -EditMode Vi
+
+
+function Test-Connection-t {
+  while ($true) {
+   Test-Connection 8.8.8.8 -Count 1
+   Start-Sleep -Seconds 1
+  }
+}
+
+function Start-Timer {
+$TaskName = [string](Get-Date -format 'HH-mm')
+$time = (Get-Date).AddMinutes((Read-Host "Enter Minutes"))
+$argument= "-Command Write-Output 'Timer up!' | Out-GridView; Read-Host 'Enter to close gui'"
+
+$t1 = New-ScheduledTaskTrigger -Once -At $time
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $argument
+$task = New-ScheduledTask -Action $action -Trigger $t1
+
+Register-ScheduledTask -Taskname $TaskName -InputObject $task
+}
