8bd2db12ed7bae3507cb147b5df0b4fa3479d72b
erik <eriksokolov@outlook.com>
2026-04-13 18:29:18 +0200

    Parameter

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,29 @@ function Upload-Git
 
 
 Set-PSReadLineOption -EditMode Vi
+
+
+function Test-Connection-t {
+  while ($true) {
+   Test-Connection 8.8.8.8 -Count 1
+   Start-Sleep -Seconds 1
+  }
+}
+
+function Start-Timer {
+  
+  param (
+    [int]$Minutes
+  )
+
+  $TaskName = [string](Get-Date -format 'HH-mm-ss-dddd')
+  $time = (Get-Date).AddMinutes($Minutes)
+  $argument = "-Command Write-Output 'Timer up!' | Out-GridView; Read-Host 'Enter to close gui'"
+
+  $t1 = New-ScheduledTaskTrigger -Once -At $time
+  $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $argument
+  $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+  $task = New-ScheduledTask -Action $action -Trigger $t1 -Settings $settings
+
+  Register-ScheduledTask -Taskname $TaskName -InputObject $task
+}
